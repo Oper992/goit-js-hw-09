87be33ea5606429531dafdc8126888e07d3afa68
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2022-02-28 20:03:44 +0200

    продолжаю

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,33 @@
-const ref = {
-    input: document.querySelector("#datetime-picker")
-}
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-console.log(ref.input)
+const ref = {
+  input: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('button[data-start]'),
+};
+
+const currentDataUnix = new Date().getTime();
+
+ref.btnStart.setAttribute('disabled', '');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDataUnix = new Date(selectedDates).getTime();
+
+    if (selectedDataUnix > currentDataUnix) {
+      ref.btnStart.removeAttribute('disabled');
+    } else {
+      window.alert('Please choose a date in the future');
+      ref.btnStart.setAttribute('disabled', '');
+    }
+    // console.log(new Date(selectedDates).getTime());
+  },
+};
+
+flatpickr(ref.input, options);
+
+// console.log(new Date());
