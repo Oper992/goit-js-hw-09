5ac21b91af24b3d514e9d125832cd6eccbaedbf0
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2022-03-06 21:26:03 +0200

    делаю

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const form = document.querySelector('.form');
+
+const generatorPromises = e => {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.elements.delay.value);
+  const step = Number(e.currentTarget.elements.step.value);
+  const amount = Number(e.currentTarget.elements.amount.value);
+  let position = 0;
+
+  const intervalId = setInterval(() => {
+    position += 1;
+
+    createPromise(position, delay)
+
+    delay += step;
+
+    if (position === amount) {
+      clearInterval(intervalId);
+    }
+  }, 0);
+};
+
+function createPromise(pos, del) {
+  setTimeout(() => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+       Promise.resolve({ pos, del });
+    } else {
+       Promise.reject({ pos, del });
+    }
+  }, del);
 }
+
+form.addEventListener('submit', generatorPromises);
